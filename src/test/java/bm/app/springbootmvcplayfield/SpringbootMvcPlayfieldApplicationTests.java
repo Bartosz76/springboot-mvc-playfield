087ec0e91d3d2dd79ae155df1bcf656719a07ba3
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-23 14:41:10 +0200

    The initial commit.

---
+++ src/test/java/bm/app/springbootmvcplayfield/SpringbootMvcPlayfieldApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.springbootmvcplayfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcPlayfieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
